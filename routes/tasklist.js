7e863f706386177b6ab3b1ee52f5cfc87cce2e5d
Yi Leung <xinshouke@sina.com>
2016-04-01 10:07:49 +0800

    Update tasklist.js

--- routes/tasklist.js
+++ routes/tasklist.js
@@ -8,63 +8,65 @@ module.exports = function (router, bodyParser) {
            console.error(err);
            return;
         }else {
-          var Task = new Array();
           files.forEach(function (file) {
-             var filePath = path.normalize(__dirname + '/../model/' + file);
+            var filePath = path.normalize(__dirname + '/../model/' + file);
              // console.log('The file is: ' , file);
-             fs.stat(filePath, function (err, stat) {
-                if(stat.isFile()) {
-                   var apipath = file.substr(0,file.length-5);
-                   router.route('/'+apipath+'/').get(function (req, res) {
-                     var Task = require.main.require("./lib/task.js");
-                     var modelpro = JSON.parse(fs.readFileSync(__dirname + '/../model/' + file,'utf-8'));
-                     var params = new Array();
-                     var j=0;
-                     for (var key in modelpro) {
-                        params[j]=key;
-                        j ++;
-                     }
-                     Task.getAll(apipath,params,function (err, results) {
-                       if (!err) res.send(results);
-                       else res.send({"error": "select tasks list failed with error code: "+err.code});
-                     });
-                   }).post(jsonParser, function (req, res) {
-                     if (!req.body) return res.sendStatus(400);
-                     var Task = require.main.require("./lib/task.js");
-                     Task.create(req.body.name, req.body.is_done, function (err, results) {
-                       if (!err) res.send(results);
-                       else res.send({"error": "create task failed with error code: "+err.code});
-                     });
-                   });   
-
-                   router.route('/'+apipath+'/:task_id').get(function (req, res) {
-                     var Task = require.main.require("./lib/task.js");
-                     Task.getById(req.params.task_id, apipath,function (err, results) {
-                       if (!err) res.send(results);
-                       else res.send({"error": "select task failed with error code: "+err.code});
-                     });
-                   }).put(jsonParser, function (req, res) {
-                     if (!req.body) return res.sendStatus(400);
-                     var params = {};
-                     if (req.body.name) params.name = req.body.name;
-                     if (req.body.is_done) params.is_done = req.body.is_done;
-                     var Task = require.main.require("./lib/task.js");
-                     Task.update(req.params.task_id, params, function (err, results) {
-                       if (!err) res.send(results);
-                       else res.send({"error": "Update task failed with error code: "+err.code});
-                     });
-                   }).delete(function (req, res) {
-                     var Task = require.main.require("./lib/task.js");
-                     Task.remove(req.params.task_id, function (err, results) {
-                     if (!err)
-                        res.send(results);
-                     else
-                        res.send({"error": "delete task failed with error code: "+err.code});
-                     });
-                   });                
-                }
+            fs.stat(filePath, function (err, stat) {         
                 if(stat.isDirectory()) {
-                   console.log(filePath + ' is: ' + 'dir');
+                   fs.readdir(filePath, function (err, files2) {
+                     files2.forEach(function (file2) {
+                        var apipath = file2.substr(0,file2.length-5);
+                        router.route('/'+file+'/'+apipath+'/').get(function (req, res) {
+                          var Task = require.main.require("./lib/task.js");
+                          var modelpro = JSON.parse(fs.readFileSync(filePath +'/'+file2,'utf-8'));
+                          var params = new Array();
+                          var j=0;
+                          for (var key in modelpro) {
+                             params[j]=key;
+                             j ++;
+                          } 
+                          Task.getAll(file,apipath,params,function (err, results) {
+                            if (!err) res.send(results);
+                            else res.send({"error": "select tasks list failed with error code: "+err.code});
+                          });
+                        }).post(jsonParser, function (req, res) {
+                          if (!req.body) return res.sendStatus(400);
+                          var Task = require.main.require("./lib/task.js");
+                          Task.create(req.body.name, req.body.is_done, function (err, results) {
+                            if (!err) res.send(results);
+                            else res.send({"error": "create task failed with error code: "+err.code});
+                          });
+                     
+                        });
+
+                        router.route('/'+apipath+'/:task_id').get(function (req, res) {
+                          var Task = require.main.require("./lib/task.js");
+                          Task.getById(req.params.task_id, apipath,function (err, results) {
+                             if (!err) res.send(results);
+                             else res.send({"error": "select task failed with error code: "+err.code});
+                          });
+                        }).put(jsonParser, function (req, res) {
+                           if (!req.body) return res.sendStatus(400);
+                           var params = {};
+                           if (req.body.name) params.name = req.body.name;
+                           if (req.body.is_done) params.is_done = req.body.is_done;
+                           var Task = require.main.require("./lib/task.js");
+                           Task.update(req.params.task_id, params, function (err, results) {
+                              if (!err) res.send(results);
+                              else res.send({"error": "Update task failed with error code: "+err.code});
+                            });
+                        }).delete(function (req, res) {
+                           var Task = require.main.require("./lib/task.js");
+                           Task.remove(req.params.task_id, function (err, results) {
+                              if (!err) res.send(results);
+                              else res.send({"error": "delete task failed with error code: "+err.code});
+                           });
+                        });                
+                      });
+                    });
+
+
+
                 }
 
              });
